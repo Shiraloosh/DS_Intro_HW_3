f44962d005396937dfe11ec4ed60dfec0cbaf7cb
Shiraloosh <100527007+Shiraloosh@users.noreply.github.com>
2022-03-23 17:44:57 +0200

    Add files via upload

---
+++ HW_3.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 20 20:48:15 2022
+
+@author: shir_al
+"""
+#   A
+n=input('Write the desirable line of the text :')
+filename=input('Wrire your file name:')
+file='C:/temp/'+filename
+
+def readline(n,file):
+    try:
+        n=int(n)
+        try:
+            fhand=open(file)
+            lines=fhand.readlines()
+            try:
+                return('" '+str(lines[n-1].rstrip())+'".')
+            except:
+                return('"'+"line "+ str(n) +" doesn't exist"+'"')
+        except:
+            return('"'+"file not found"+'"')
+    except:
+        return('"'+"invalid input detected"+'"')
+
+print(readline(n,file))
+
+#  B
+# ex3_text.txt
+filename=input('Wrire your file name:')
+file='C:/temp/'+filename
+def longest_words(file):
+   
+    try:
+        fhand=open(file)  
+        data=fhand.read()
+        data=data.replace('  ', ' ')
+        data=data.replace('-', '')
+        data=data.replace('.', ' ')
+        data=data.replace(',', ' ')
+        data=data.replace('(', ' ')
+        data=data.replace(')', ' ')       
+        words1=data.split()
+        sortedwords=sorted(words1,key=len,reverse=True)
+                                                                      
+        return(sortedwords[:5])
+       
+    except:
+         return('"'+"file not found"+'"')
+    
+      
+    
+print(longest_words(file))
+    
+    
+    
